fda24711e724966f9eff5bbcf4e2e229b4ce317d
Clau <claudia.rojas.aramayo@gmail.com>
2025-04-11 10:21:21 -0300

    act
    ac

---
+++ Unity-Android-BLE-SendReceive 2025/Assets/Codigos/AçãoLigar.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AçãoLigar : MonoBehaviour
+{
+    private bool ligadoOn = false;  // Inicializa o estado como desligado
+    [SerializeField] Sprite botaoOFF;
+    [SerializeField] Sprite botaoON;
+
+    void Start()
+    {
+        OnOff();  // Ajusta o estado do botão ao iniciar
+    }
+
+    public void OnOff()
+    {
+        if (ligadoOn)
+        {
+            // Se estiver ligado, mostra a imagem OFF e oculta a imagem ON
+            gameObject.GetComponent<Image>().sprite = botaoOFF;
+            
+        }
+        else
+        {
+            // Se estiver desligado, mostra a imagem ON e oculta a imagem OFF
+            gameObject.GetComponent<Image>().sprite = botaoON;
+        }
+    }
+
+    // Função chamada ao clicar no botão
+    public void Toggle()
+    {
+        ligadoOn = !ligadoOn;  // Alterna o estado de ligadoOn
+        OnOff();  // Atualiza o estado do botão
+    }
+}
